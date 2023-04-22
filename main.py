c349a2e6bc473daa8cf8066387c8a4a5e23f899f
Leps <leps@linux.internal>
2023-04-22 08:40:08 +0000

    separator

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ f = open("data.txt", "r")
 d = f.read()
 d = d.split()
 
-output = json.dumps([[row, 1] for row in d], ensure_ascii=False)
+output = json.dumps([[row, 1] for row in d], ensure_ascii=False, separators=(',', ':'))
 print(output)
 
 fw = open("output.json", "w")
