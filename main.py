0febfd57656107e8a1980d743a325912ec49907f
Leps <leps@linux.internal>
2023-04-22 08:38:44 +0000

    output

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ f = open("data.txt", "r")
 d = f.read()
 d = d.split()
 
-print(json.dumps([[row, 1] for row in d], ensure_ascii=False))
+output = json.dumps([[row, 1] for row in d], ensure_ascii=False)
+print(output)
+
+fw = open("output.json", "w")
+fw.write(output)
+
+f.close()
+fw.close()
